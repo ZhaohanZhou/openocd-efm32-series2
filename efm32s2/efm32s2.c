6559068f803c0f83bb853986035fd98a4129154b
Fred.Zhou <fred.zhou@myriota.com>
2023-05-02 21:56:39 +0930

    Improved the support of user data flash

--- efm32s2/efm32s2.c
+++ efm32s2/efm32s2.c
@@ -361,7 +361,7 @@ static int efm32x_read_info(struct flash_bank *bank)
 			efm32x_info->reg_lock = EFM32_MSC_REG_LOCK;
 			ret = efm32x_get_part_info(bank, efm32_info);
 			if (ret != ERROR_OK)
-				return ret;			
+				return ret;
 			break;
 	}
 
@@ -1126,15 +1126,6 @@ static int efm32x_probe(struct flash_bank *bank)
 
 	if (bank->base == 0) bank->base = base_address;
 
-	LOG_INFO("detected part: %cG%d%c%03d, rev %d",
-			efm32_mcu_info->part_family,
-			efm32_mcu_info->part_family_num,
-			efm32_mcu_info->dev_num_letter,
-			efm32_mcu_info->dev_num_digits,
-			efm32_mcu_info->prod_rev);
-	LOG_INFO("flash size = %dkbytes", efm32_mcu_info->flash_sz_kib);
-	LOG_INFO("flash page size = %dbytes", efm32_mcu_info->page_size);
-
 	assert(efm32_mcu_info->page_size != 0);
 
 	free(bank->sectors);
@@ -1156,6 +1147,7 @@ static int efm32x_probe(struct flash_bank *bank)
 		return ret;
 	}
 
+	uint16_t page_size;
 	if (bank->base == base_address) {
 		bank->num_sectors = efm32_mcu_info->flash_sz_kib * 1024 /
 			efm32_mcu_info->page_size;
@@ -1166,14 +1158,26 @@ static int efm32x_probe(struct flash_bank *bank)
 			LOG_ERROR("Failed to read LB data");
 			return ret;
 		}
-	} else
+		page_size = efm32_mcu_info->page_size;
+	} else{
 		bank->num_sectors = 1;
-	bank->size = bank->num_sectors * efm32_mcu_info->page_size;
-	bank->sectors = malloc(sizeof(struct flash_sector) * bank->num_sectors);
+		page_size = 1024;
+	}
+	bank->size = bank->num_sectors * page_size;
 
+	LOG_INFO("detected part: %cG%d%c%03d, rev %d",
+	efm32_mcu_info->part_family,
+	efm32_mcu_info->part_family_num,
+	efm32_mcu_info->dev_num_letter,
+	efm32_mcu_info->dev_num_digits,
+	efm32_mcu_info->prod_rev);
+	LOG_INFO("flash size = %dbytes", bank->size);
+	LOG_INFO("flash page size = %dbytes", page_size);
+
+	bank->sectors = malloc(sizeof(struct flash_sector) * bank->num_sectors);
 	for (uint32_t i = 0; i < bank->num_sectors; i++) {
-		bank->sectors[i].offset = i * efm32_mcu_info->page_size;
-		bank->sectors[i].size = efm32_mcu_info->page_size;
+		bank->sectors[i].offset = i * page_size;
+		bank->sectors[i].size = page_size;
 		bank->sectors[i].is_erased = -1;
 		bank->sectors[i].is_protected = 1;
 	}
